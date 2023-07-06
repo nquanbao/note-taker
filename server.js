20bbe822bb2a168178f436e7dc91da498a03e54b
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-07-07 02:25:40 +0700

    fix some code, and tried to code for delete part

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs  = require ('fs');
 const express = require ('express');
 const path = require ('path')
 const db = require ('./db/db.json');
+const uuid = require ('./public/assets/js/uuid')
 const app = express();
 const PORT = 3001;
 
@@ -14,12 +15,24 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     res.json (db);
 })
+// app.delete ('/api/notes/:id', (req, res) => {
+//     const idEL = req.body.id;
+//     for (let i = 0; i < db.length; i++){
+//         const id = db[i].id;
+//         if(id === idEL){
+//             delete db[i];
+//             return db;
+//         }
+//     }
+    
+// })
 app.post('/api/notes', (req, res) => {
     const {title, text} = req.body;
     if (title && text) {
         const newNote = {
             title,
-            text
+            text,
+            id: uuid(),
         };
         db.push(newNote)
         const NoteString = JSON.stringify(db)
