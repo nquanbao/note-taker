b4decc1c9014443f23326a48e3d16467a5b41379
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-07-07 10:19:47 +0700

    fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require ('path')
 const db = require ('./db/db.json');
 const uuid = require ('./public/assets/js/uuid')
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
